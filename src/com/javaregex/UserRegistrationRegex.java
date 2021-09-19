8ed2fe26ade71f39c6d26ded356228924b1ef731
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-19 14:42:50 +0530

    UC4 has been created where mobile number is validated

--- src/com/javaregex/UserRegistrationRegex.java
+++ src/com/javaregex/UserRegistrationRegex.java
@@ -3,6 +3,10 @@ package com.javaregex;
 import java.util.*;
 import java.util.regex.Pattern;
 
+/**
+ * @author saneeths
+ *class where validation of the user registration is done using java regex
+ */
 public class UserRegistrationRegex {
 	public static Scanner scanner = new Scanner(System.in);
 	
@@ -11,23 +15,33 @@ public class UserRegistrationRegex {
 		firstName();
 		lastName();
 		email();
+		mobileNo();
 		
 	}
 	
+	/**
+	 * method where first name of the user is validated
+	 */
 	public static void firstName() {
-		String patternFirstName = "[A-Z]{1}[a-zA-Z]{2,}";
+		String patternFirstName = "^[A-Z]{1}[a-zA-Z]{2,}";
 		System.out.println("Enter first name");
 		String firstName = scanner.nextLine();
 		validator(patternFirstName, firstName, "First Name");
 	}
 	
+	/**
+	 * method where last name of the user validated
+	 */
 	public static void lastName() {
-		String patternLastName = "[A-Z]{1}[a-zA-Z]{2,}";
+		String patternLastName = "^[A-Z]{1}[a-zA-Z]{2,}";
 		System.out.println("Enter last name");
 		String lastName = scanner.nextLine();
 		validator(patternLastName, lastName, "Last Name");
 	}
 	
+	/**
+	 * method where email of the user is validated
+	 */
 	public static void email() {
 		String patternEmail = "^[a-zA-Z0-9-_+]+(\\.?[a-zA-Z0-9-_]+)@[a-zA-Z0-9-_]+\\.[a-zA-Z]{2,}(\\.?[a-zA-Z-_]+)";
 		System.out.println("Enter email");
@@ -35,6 +49,21 @@ public class UserRegistrationRegex {
 		validator(patternEmail, email, "Email");
 	}
 	
+	/**
+	 * method where mobile number of the user is validated
+	 */
+	public static void mobileNo() {
+		String patternMobileNo = "^[0-9]{2}[\\s][0-9]{10}";
+		System.out.println("Enter Mobile No.");
+		String mobileNo = scanner.nextLine();
+		validator(patternMobileNo, mobileNo, "Mobile Number");
+	}
+	
+	/**method where the pattern is matched with input
+	 * @param pattern
+	 * @param input
+	 * @param type
+	 */
 	public static void validator(String pattern, String input, String type) {
 		if(Pattern.matches(pattern, input)) {
 			System.out.println(input+" is valid "+type);
