9b75bbe45d461d5cd3f6bbd0e0918bbdfc9c1747
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-19 13:23:14 +0530

    Welcome page is create d

---
+++ src/com/javaregex/UserRegistrationRegex.java
@@ -0,0 +1,8 @@
+package com.javaregex;
+
+public class UserRegistrationRegex {
+	public static void main(String[] args) {
+		System.out.println("Welcome to java regex user registration problem");
+		
+	}
+}
